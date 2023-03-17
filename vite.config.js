058a1152fb49d1dd8031ac39bdde1e6fa82c17fa
JD Singh <jagdeepsingh@founderandlightning.com>
2023-03-18 04:57:32 +0530

    Tailwind + Alpine.js + Add Livewire style+scripts

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: "200.20.1.1",
+        watch: {
+            ignored: [
+                "!**/vendor/**"
+            ]
+        },
+        port: 5173
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
