20870d13594d2d0a3076e7346266cb78672ddf05
JD Singh <jagdeepsingh@founderandlightning.com>
2023-04-03 04:23:34 +0530

    Dark mode + Vite IP:port changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: "200.20.1.6",
+        watch: {
+            ignored: [
+                "!**/vendor/**"
+            ]
+        },
+        port: 5173
+    },
     plugins: [
         laravel({
             input: [
