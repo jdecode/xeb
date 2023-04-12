659208dbc26acd3b6db01da7ee89be977f5fd5c4
JD Singh <jagdeepsingh@founderandlightning.com>
2023-04-13 01:33:20 +0530

    IDE-suggested changes

--- tests/Feature/Admin/AdminTest.php
+++ tests/Feature/Admin/AdminTest.php
@@ -12,7 +12,7 @@ beforeEach(function () {
 
 test('admin login form can be rendered', function () {
     $response = $this->get(route('admin.loginForm'));
-    $response->assertStatus(200);
+    $response->assertOk();
     $response->assertViewIs('admin.loginForm');
 })->group('admin', 'admin-login-form');
 
@@ -21,31 +21,31 @@ test('admin login form can be submitted', function () {
         'email' => 'admin@admin.com',
         'password' => 'admin',
     ]);
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertRedirect(route('admin.dashboard'));
 })->group('admin', 'admin-login-form');
 
 test('admin dashboard can be rendered', function () {
     $response = $this->actingAs(Admin::first(), 'admin')->get(route('admin.dashboard'));
-    $response->assertStatus(200);
+    $response->assertOk();
     $response->assertViewIs('admin.dashboard');
 })->group('admin', 'admin-dashboard');
 
 test('admin can logout', function () {
     $response = $this->actingAs(Admin::first(), 'admin')->post(route('admin.logout'));
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertRedirect(route('admin.loginForm'));
 })->group('admin', 'admin-logout');
 
 test('redirect to dashboard if already logged in', function () {
     $response = $this->actingAs(Admin::first(), 'admin')->get(route('admin'));
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertRedirect(route('admin.dashboard'));
 })->group('admin', 'admin-login-form');
 
 test('admin redirect to login form if not logged in', function () {
     $response = $this->get(route('admin.dashboard'));
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertRedirect(route('admin'));
 })->group('admin', 'admin-dashboard');
 
@@ -54,7 +54,7 @@ test('cannot login with invalid credentials', function () {
         'email' => 'admin@admin.com',
         'password' => 'invalid',
     ]);
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertRedirect(route('admin.loginForm'));
     $response->assertSessionHasErrors('email');
 })->group('admin', 'admin-login-form');
@@ -63,14 +63,13 @@ test('invalid credentials', function (array $credentials) {
     $this->admin = [...$this->admin, ...$credentials];
     $response = $this->post(route('admin.login'), $this->admin);
     $response->assertRedirect(route('home'));
-    $response->assertStatus(302);
+    $response->assertFound();
     $response->assertSessionHasErrors([...array_keys($credentials)]);
-})->group('admin', 'admin-login-form', 'dataset')
-    ->with(
-        [
-            'Empty email' => [['email' => '']],
-            'Empty password' => [['password' => '']],
-            'Invalid email' => [['email' => 'invalid']],
-            'Invalid email and password' => [['email' => 'invalid', 'password' => '']],
-        ]
-    );
+})->with(
+    [
+        'Empty email' => [['email' => '']],
+        'Empty password' => [['password' => '']],
+        'Invalid email' => [['email' => 'invalid']],
+        'Invalid email and password' => [['email' => 'invalid', 'password' => '']],
+    ]
+)->group('admin', 'admin-login-form', 'dataset');
