4bb32b6b928bde8fb6d5ab27b5c191c9729edc67
JD Singh <jagdeepsingh@founderandlightning.com>
2023-04-13 23:22:39 +0530

    User logout test-case + refactoring

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -4,30 +4,38 @@ use App\Models\User;
 use App\Providers\RouteServiceProvider;
 
 test('login screen can be rendered', function () {
-    $response = $this->get('/login');
+    $response = $this->get(route('login'));
 
-    $response->assertStatus(200);
-});
+    $response->assertOk();
+})->group('auth', 'login');
 
 test('users can authenticate using the login screen', function () {
     $user = User::factory()->create();
 
-    $response = $this->post('/login', [
+    $response = $this->post(route('login'), [
         'email' => $user->email,
         'password' => 'password',
     ]);
 
     $this->assertAuthenticated();
     $response->assertRedirect(RouteServiceProvider::HOME);
-});
+})->group('auth', 'login');
 
 test('users can not authenticate with invalid password', function () {
     $user = User::factory()->create();
 
-    $this->post('/login', [
+    $this->post(route('login'), [
         'email' => $user->email,
         'password' => 'wrong-password',
     ]);
 
     $this->assertGuest();
-});
+})->group('auth', 'login');
+
+test('User can logout', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('logout'));
+
+    $response->assertRedirect(route('home'));
+})->group('auth', 'logout');
